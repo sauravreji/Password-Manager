beabd14f6518d4a068ed79b6507d191b966dd0ca
Saurav Kurian Reji <sauravkreji@gmail.com>
2024-11-30 18:45:01 +0530

    added decryption  and save password

--- window.py
+++ window.py
@@ -7,9 +7,10 @@ class passwordManager:
     def __init__(self, root):
         self.root = root
         self.root.title("Password Manager")
-        self.root.geometry("400x200")
+        self.root.geometry("1000x1000")
         self.generated_password = None
         self.encPassword = None
+        self.decPassword = None
         self.key = Fernet.generate_key()
 
         self.fernet = Fernet(self.key)
@@ -18,11 +19,31 @@ class passwordManager:
         
 
 
-        self.myButton = Button(self.root, text="Generate password", command=self.generatePassword)
-        self.myTextfield = Entry(self.root, width=30)
+        self.gp_button = Button(self.root, text="Generate password", command=self.generatePassword)
+        self.sp_button = Button(self.root, text="Save password")
+        self.password_testfield = Entry(self.root, width=30,fg = "grey")
+        self.link_textfield = Entry(self.root,width = 30,fg = "grey")
+        self.set_placeholder(self.link_textfield, "Please enter the link")
+        self.set_placeholder(self.password_testfield, "Password")
 
-        self.myTextfield.grid(row=0, column=0, padx=10, pady=20)
-        self.myButton.grid(row=0, column=1, padx=10, pady=20)
+        self.link_textfield.grid(row=0, column=0, padx=10, pady=20)
+        self.password_testfield.grid(row=0, column=1, padx=10, pady=20)
+        self.gp_button.grid(row=0, column=2, padx=10, pady=20)
+        self.sp_button.grid(row = 0,column = 3,padx = 10,pady = 20)
+
+    def set_placeholder(self, field, placeholder_text):
+        field.insert(0,placeholder_text)
+
+        def on_focus_in(event):
+            if field.get() == placeholder_text:
+                field.delete(0,END)
+                field.config(fg = "white")
+        def on_focus_out(event):
+            if not field.get():
+                field.insert(0,placeholder_text)
+                field.config(fg = "grey")
+        field.bind("<FocusIn>", on_focus_in)
+        field.bind("<FocusOut>", on_focus_out)
 
     def generatePassword(self):
         lower = string.ascii_lowercase 
@@ -51,16 +72,20 @@ class passwordManager:
         
 
        
-        self.myTextfield.delete(0, END)
-        self.myTextfield.insert(0, ''.join(password))
+        self.password_testfield.delete(0, END)
+        self.password_testfield.insert(0, ''.join(password))
         self.generated_password = ''.join(password)
 
         
 
         self.encPassword = self.fernet.encrypt(self.generated_password.encode())
-
+        self.decPassword  = self.fernet.decrypt(self.encPassword).decode()
+        self.password_testfield.config(fg = "white")
         print("Orginal String: ",self.generated_password)
         print("Encrypted string",self.encPassword)
+        print("Decrypted string",self.decPassword)
+
+
 
 if __name__ == "__main__":
     root = Tk()
