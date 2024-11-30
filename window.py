759b3524dcc09abb2de86556a0f891c2e8c3c1e0
Saurav Kurian Reji <sauravkreji@gmail.com>
2024-11-30 11:13:34 +0530

    Encryption is added for security

--- window.py
+++ window.py
@@ -1,12 +1,22 @@
 from tkinter import *
 import random
 import string
+from cryptography.fernet import Fernet
 
 class passwordManager:
     def __init__(self, root):
         self.root = root
         self.root.title("Password Manager")
         self.root.geometry("400x200")
+        self.generated_password = None
+        self.encPassword = None
+        self.key = Fernet.generate_key()
+
+        self.fernet = Fernet(self.key)
+
+        
+        
+
 
         self.myButton = Button(self.root, text="Generate password", command=self.generatePassword)
         self.myTextfield = Entry(self.root, width=30)
@@ -15,12 +25,12 @@ class passwordManager:
         self.myButton.grid(row=0, column=1, padx=10, pady=20)
 
     def generatePassword(self):
-        lower = string.ascii_lowercase  # 'abcdefghijklmnopqrstuvwxyz'
-        upper = string.ascii_uppercase  # 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        digits = string.digits          # '0123456789'
-        special = string.punctuation    # '!@#$%^&*()_+-=[]{}|;:,.<>?/'
+        lower = string.ascii_lowercase 
+        upper = string.ascii_uppercase  
+        digits = string.digits          
+        special = string.punctuation   
 
-        # Ensure the password contains at least one of each type of character
+        
         all_characters = lower + upper + digits + special
         password = [
             random.choice(lower),
@@ -38,9 +48,19 @@ class passwordManager:
        
         random.shuffle(password)
 
+        
+
        
         self.myTextfield.delete(0, END)
         self.myTextfield.insert(0, ''.join(password))
+        self.generated_password = ''.join(password)
+
+        
+
+        self.encPassword = self.fernet.encrypt(self.generated_password.encode())
+
+        print("Orginal String: ",self.generated_password)
+        print("Encrypted string",self.encPassword)
 
 if __name__ == "__main__":
     root = Tk()
